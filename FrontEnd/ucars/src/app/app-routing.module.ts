3cbc5c90ee3e8a219846c08f08e28bdc0c5ac80a
Thanhtk1 <thanhtk1@scb.com.vn>
2022-07-14 11:35:50 +0700

    - Add Cars Page, Navigation to cars page

--- FrontEnd/ucars/src/app/app-routing.module.ts
+++ FrontEnd/ucars/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrandListComponent } from './Layout/brand-list/brand-list.component';
 import { BrandUpdateComponent } from './Layout/brand-update/brand-update.component';
+import { CarListComponent } from './Layout/car-list/car-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/brandslist', pathMatch: 'full' },
   { path: 'brandslist', component: BrandListComponent },
-  { path: 'brand/:id', component: BrandUpdateComponent }
+  { path: 'brand/:id', component: BrandUpdateComponent },
+  { path: 'cars', component: CarListComponent }
 ];
 
 @NgModule({
